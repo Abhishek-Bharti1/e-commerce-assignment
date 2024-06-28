fcc6b6fe4584437b086d5ca31e4fd1188fb692fb
Abhishek Bharti <abhishekbharti91550@gmail.com>
2024-06-28 11:57:55 +0530

    error fix

--- backend/src/utils/features.ts
+++ backend/src/utils/features.ts
@@ -1,46 +1,10 @@
-import { Redis } from "ioredis";
-import { redis } from "../app.js";
-import { InvalidateCacheProps} from "../types/types.js";
 import mongoose from "mongoose";
 
-export const connectRedis = (redisURI: string) => {
-  const redis = new Redis(redisURI);
-
-  redis.on("connect", () => console.log("Redis Connected"));
-  redis.on("error", (e) => console.log(e));
-
-  return redis;
-};
-
 export const connectDB = (uri: string) => {
-  mongoose
-    .connect(uri, {
-      dbName: "Ecommerce_24",
-    })
-    .then((c) => console.log(`DB Connected to ${c.connection.host}`))
-    .catch((e) => console.log(e));
-};
-
-export const invalidateCache = async ({
-  product,
-  productId,
-}: InvalidateCacheProps) => {
-
-
-  if (product) {
-    const productKeys: string[] = [
-      "latest-products",
-      "categories",
-      "all-products",
-    ];
-
-    if (typeof productId === "string") productKeys.push(`product-${productId}`);
-
-    if (typeof productId === "object")
-      productId.forEach((i) => productKeys.push(`product-${i}`));
-
-    await redis.del(productKeys);
-  }
-
-};
-
+    mongoose
+      .connect(uri, {
+        dbName: "e-commerce-assignment",
+      })
+      .then((c) => console.log(`DB Connected to ${c.connection.host}`))
+      .catch((e) => console.log(e));
+  };
